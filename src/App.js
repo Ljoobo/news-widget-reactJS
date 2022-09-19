f971e43570ef46b1e828e8dc8d17a80fa26426e9
Ljoobo <ljubojevic.dario@gmail.com>
2022-09-19 16:49:18 +0200

    First push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Menu from 'components/Menu';
+import News from 'components/News';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world
+      <Menu />
+      <News />
+    </div>
+  );
+}
+
+export default App;
